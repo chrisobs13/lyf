fa1434b77fb427f8b1fa3e511e5937d24407bf9f
Chris OBrien <chris@hellodative.com>
2013-12-12 14:27:42 -0500

    add confirm and thank you page, add analytics, final qa

--- Build/assets/js/app.js
+++ Build/assets/js/app.js
@@ -1,5 +1,21 @@
 // Site Javascripts
 
-jQuery(document).ready(function($) {
-	
+$(document).ready(function(){
+  
+	$('.track-twitter').on('click', function() {
+		ga('send', 'event', 'twitter', 'click');
+	});
+
+	$('.track-facebook').on('click', function() {
+		ga('send', 'event', 'facebook', 'click');
+	});
+
+	$('.track-pinterest').on('click', function() {
+		ga('send', 'event', 'pinterest', 'click');
+	});
+
+	$('.track-instagram').on('click', function() {
+		ga('send', 'event', 'instagram', 'click');
+	});		
+
 });
